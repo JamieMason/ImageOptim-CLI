cae16730de9c181447e665d8477acf7f88b0f47d
Jamie Mason <siunmaru@gmail.com>
2013-07-28 14:31:01 +0100

    add % quality loss, refs #30

--- src/tasks/gather-results.js
+++ src/tasks/gather-results.js
@@ -12,83 +12,118 @@ module.exports = function(grunt) {
   grunt.registerTask('gather', 'Gather results from file system.', function() {
 
     var done = this.async();
-    var images = toAbsolutePath('../images');
-    var imageNames = [];
-    var tools = [
-      'codekit',
-      'grunt-contrib-imagemin',
-      'imageoptim-cli',
-      'imageoptim-cli-jpegmini',
-      'smushit',
-      'tinypng'
-    ];
+    var imageDirectory = toAbsolutePath('../images');
 
-    var failed = [];
+    // get absolute paths to all files under /images
+    exec('find ' + imageDirectory + ' -type f -exec echo {} \\;')
 
-    exec('find ' + images + ' -type f -exec echo {} \\;')
-      .then(function(res) {
-        return _(res[0].split('\n'))
-          .filter(function(path) {
-            return path && path.search(/\.(gif|jpg|jpeg|png)/) !== -1;
-          })
-          .map(function(imgPath) {
-            var branches = imgPath.split('/');
-            return {
-              path: imgPath,
-              image: branches[branches.length - 1],
-              tool: branches[branches.length - 2],
-              size: void(0),
-              qualityLoss: void(0)
-            };
-          })
-          .value();
+    .then(function(res) {
+
+      return _(res[0].split('\n'))
+
+      // remove eg .DS_Store files
+      .filter(function(path) {
+        return path && path.search(/\.(gif|jpg|jpeg|png)/) !== -1;
       })
-      .then(function(data) {
-        return data.reduce(function(memo, img) {
-          return memo.then(function() {
-            return exec('stat -f %z ' + img.path).then(function(res) {
-              var size = res[0];
-              img.size = parseInt(size, 10);
-              return data;
-            });
+
+      // convert image paths into objects
+      .map(function(imgPath) {
+        var branches = imgPath.split('/');
+        return {
+          path: imgPath,
+          image: branches[branches.length - 1],
+          tool: branches[branches.length - 2],
+          size: void(0),
+          meanErrorSquared: void(0)
+        };
+      })
+
+      .value();
+
+    })
+
+    // get the file sizes of each image
+    .then(function(collection) {
+      return collection.reduce(function(memo, img) {
+        return memo.then(function() {
+          return exec('stat -f %z ' + img.path).then(function(res) {
+            img.size = parseInt(res[0], 10);
+            return collection;
           });
-        }, q(null));
-      })
-      .then(function(data) {
-        return data.reduce(function(memo, img, i) {
-          return memo.then(function() {
-            var command = 'compare -metric MSE ' + images + '/photoshop/' + img.image + ' ' + img.path + ' /dev/null';
-            return exec(command)
-              .then(function(res) {
-                if (!res || !res[1]) {
-                  throw new Error('no output for ' + command);
-                }
-                img.qualityLoss = parseInt(res[1].replace(/ \(.+/, ''), 10);
-                return data;
-              }, function(err) {
-                var loss = err.toString().replace('Error: Command failed: ', '').replace(/ \(.+/, '');
-                img.qualityLoss = parseFloat(loss, 10);
-                return data;
-              });
-          });
-        }, q(null));
-      })
-      .then(function (data) {
-        return data.map(function (img) {
-          delete img.path;
-          return img;
         });
-      })
-      .then(function (data) {
-        return writeFile('raw-results.json', JSON.stringify(data, null, 2));
-      })
-      .done(function() {
-        console.log('SUCCESS');
-        done();
-      }, function(err) {
-        console.error('FAILED:', err);
-        done();
+      }, q(null));
+    })
+
+    // compare the difference between the optimised image and the original
+    .then(function(collection) {
+      return collection.reduce(function(memo, img, i) {
+        return memo.then(function() {
+          var command = 'compare -metric MSE ' + imageDirectory + '/photoshop/' + img.image + ' ' + img.path + ' /dev/null';
+          return exec(command)
+            .then(function(res) {
+              if (!res || !res[1]) {
+                throw new Error('no output for ' + command);
+              }
+              img.meanErrorSquared = parseInt(res[1].replace(/ \(.+/, ''), 10);
+              return collection;
+            }, function(err) {
+              var loss = err.toString().replace('Error: Command failed: ', '').replace(/ \(.+/, '');
+              img.meanErrorSquared = parseFloat(loss, 10);
+              return collection;
+            });
+        });
+      }, q(null));
+    })
+
+    // calculate % loss
+    .then(function(collection) {
+      var worstQuality = {};
+      var results = [];
+
+      // separe worst from tools
+      _.each(collection, function(img) {
+        if (img.tool === 'worst') {
+          worstQuality[img.image] = img.meanErrorSquared;
+        } else {
+          results.push(img);
+        }
       });
+
+      // calculate loss
+      _.each(results, function(img) {
+        var imgLoss = img.meanErrorSquared;
+        var worstLoss = worstQuality[img.image];
+        var percentLoss = (imgLoss / worstLoss) * 100;
+
+        // 0 / 0 will be NaN
+        percentLoss = percentLoss || 0;
+
+        img.qualityLoss = parseFloat(percentLoss.toFixed(2));
+      });
+
+      return results;
+    })
+
+    // remove path property
+    .then(function(collection) {
+      return collection.map(function(img) {
+        delete img.path;
+        return img;
+      });
+    })
+
+    // write to JSON
+    .then(function(collection) {
+      return writeFile('raw-results.json', JSON.stringify(collection, null, 2));
+    })
+
+    .done(function() {
+      console.log('SUCCESS');
+      done();
+    }, function(err) {
+      console.error('FAILED:', err);
+      done();
+    });
 
   });
 
