b44c07862d8b4267c5f2ef5ed896b707b70c2f4d
Jamie Mason <jamie@foldleft.io>
2018-12-23 18:34:29 +0000

    feat(cli): only output summary when savings are made

--- src/write-report.ts
+++ src/write-report.ts
@@ -1,10 +1,14 @@
 import { IStats } from './get-stats';
-import { result } from './log';
+import { result, warning } from './log';
 
 export const writeReport = async (stats: IStats) => {
   const { total } = stats;
   const results = stats.files.concat(total);
-  results.forEach(({ path, pretty, raw }) => {
-    result(path, pretty.before, pretty.after, pretty.saving, raw.percentSaving, 100);
-  });
+  if (total.pretty.saving === '0B') {
+    warning('No size savings');
+  } else {
+    results.forEach(({ path, pretty, raw }) => {
+      result(path, pretty.before, pretty.after, pretty.saving, raw.percentSaving, 100);
+    });
+  }
 };
